b030d446bb9fb313b07a6a32b7766d324715d0d1
Sebastian Holtz <holtz@make-better.de>
2016-04-15 23:23:56 +0200

    die datei liste wird zusammengebaut

--- check_files.py
+++ check_files.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+from pprint import pprint
 
 file_name = os.path.basename(sys.argv[0])
 
@@ -9,6 +10,21 @@ excluded_files = [
     file_name, # Das eigene Script sollte immer ausgeschlossen werden
     ".travis.yml",
     ".gitignore",
+    "README.md",
     ".git",
+    ".idea",# wird nur für die Entwicklung gebraucht
 ]
 
+excluded_files = ["./" + f for f in excluded_files]
+
+files_for_check = []
+root_dir = '.'
+for dir_name, subdir_list, file_list in os.walk(root_dir):
+    if dir_name in excluded_files:
+        if len(subdir_list) > 0:
+            del subdir_list[:]
+        continue
+    for f_name in file_list:
+        f_name = dir_name + '/' + f_name
+        if f_name not in excluded_files:
+            files_for_check.append(f_name)
